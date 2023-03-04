dd3184d6b59c400eaeeee93e75e4eb6caebd7c4e
Handoko <hd.doko22@gmail.com>
2023-03-04 16:36:03 +0700

    tampilkan data by id

--- controller.js
+++ controller.js
@@ -7,7 +7,7 @@ exports.index = function(req, res){
     response.ok("Aplikasi REST API Book Berjalan", res);
 };
 
-// Menampilkan Semua Data Dari Database
+// Menampilkan Semua Data Buku
 exports.tampilData = function(req, res){
   connection.query('SELECT * FROM `tb_buku`', function(error, rows, fields){
     if (error) {
@@ -16,4 +16,16 @@ exports.tampilData = function(req, res){
         response.ok(rows, res);
     }
   });  
+};
+
+// Menampilkan Data Buku Berdasarkan ID
+exports.tampilDataByID = function(req, res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM `tb_buku` WHERE `id` = ?', [id] ,function(error, rows, fields){
+        if (error) {
+            console.log(error);
+        } else{
+            response.ok(rows, res);
+        }
+    });
 };
