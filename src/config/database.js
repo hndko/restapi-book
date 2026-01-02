bb3dfb555f68424edcff5a7016c0c51679040180
hndko <hd.doko22@gmail.com>
2026-01-02 16:08:57 +0700

    chore: update default database password to an empty string

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ const mysql = require("mysql");
 const conn = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "root",
+  password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "db_restapi",
 });
 
